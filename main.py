c882e594027ceec4bb7260bc6641d2b9f7e4c051
Vladimir Dobrovolskii <v.dobrovolskii@hotmail.com>
2021-10-20 16:50:16 +0300

    add icon and window title

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from coref_markup.application import Application
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.iconphoto(False, tk.PhotoImage(file='icon.png'))
+    root.title("Coref Markup")
     app = Application(root)
     ttk.Style().theme_use({"Windows": "winnative", "Darwin": "aqua"}.get(platform.system(), "default"))
     root.resizable(False, False)
